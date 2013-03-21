64de50615a63360530676a226368b3d555e4c1dd
William Wedler <wwedler@riseup.net>
2013-03-20 20:38:19 -0400

    initial application script

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'label_gen', :git => 'git://github.com/zflat/label_gen.git'
---
+++ config/initializers/label_gen.rb
@@ -0,0 +1,4 @@
+LabelGen.configure do |config|
+  config.max_number_used = 1600
+  config.locale_path = File.dirname(__FILE__) + '/../../locales'
+end
