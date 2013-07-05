5da83924a560615ed4125536724a7c125cfe6e40
William Wedler <wwedler@riseup.net>
2013-07-04 20:30:54 -0400

    configure to use plain label

--- config/initializers/label_gen.rb
+++ config/initializers/label_gen.rb
@@ -1,6 +1,7 @@
 LabelGen.configure do |config|
-  config.max_number_used = 1600
+  config.max_number_used = 1800
   config.qr_url = "http://qr.freeridepgh.org/bikes/%{number}"
   config.number_label = "%<number>.05d"
+  config.template_name = "Ol875Plain"
   config.output_path = File.expand_path(File.join(File.dirname(__FILE__),'..', '..', 'tmp', 'bike_labels.pdf'))
 end
